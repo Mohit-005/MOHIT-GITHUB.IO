e9b5dee8bf2e1a1fb4237c62318f05b7cfe9bc06
Mohit Kumar <115066003+Mohit-005@users.noreply.github.com>
2025-04-25 12:24:45 +0530

    Refactor Clock component to use functional components and hooks for improved performance and readability

--- components/util components/clock.js
+++ components/util components/clock.js
@@ -1,50 +1,49 @@
-import { Component } from 'react'
+import { useState, useEffect } from 'react'
+import dynamic from 'next/dynamic'
 
-export default class Clock extends Component {
-    constructor() {
-        super();
-        this.month_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        this.day_list = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-        this.state = {
-            hour_12: true,
-            current_time: new Date()
-        };
-    }
+const Clock = ({ onlyTime, onlyDay }) => {
+    const [displayTime, setDisplayTime] = useState('');
+    const month_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const day_list = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    componentDidMount() {
-        this.update_time = setInterval(() => {
-            this.setState({ current_time: new Date() });
-        }, 10 * 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.update_time);
-    }
-
-    render() {
-        const { current_time } = this.state;
-
-        let day = this.day_list[current_time.getDay()];
+    const updateTime = () => {
+        const current_time = new Date();
+        let day = day_list[current_time.getDay()];
         let hour = current_time.getHours();
         let minute = current_time.getMinutes();
-        let month = this.month_list[current_time.getMonth()];
+        let month = month_list[current_time.getMonth()];
         let date = current_time.getDate().toLocaleString();
         let meridiem = (hour < 12 ? "AM" : "PM");
 
         if (minute.toLocaleString().length === 1) {
-            minute = "0" + minute
+            minute = "0" + minute;
         }
 
-        if (this.state.hour_12 && hour > 12) hour -= 12;
+        if (hour > 12) hour -= 12;
 
-        let display_time;
-        if (this.props.onlyTime) {
-            display_time = hour + ":" + minute + " " + meridiem;
+        let time;
+        if (onlyTime) {
+            time = hour + ":" + minute + " " + meridiem;
         }
-        else if (this.props.onlyDay) {
-            display_time = day + " " + month + " " + date;
+        else if (onlyDay) {
+            time = day + " " + month + " " + date;
         }
-        else display_time = day + " " + month + " " + date + " " + hour + ":" + minute + " " + meridiem;
-        return <span>{display_time}</span>;
-    }
-}
+        else {
+            time = day + " " + month + " " + date + " " + hour + ":" + minute + " " + meridiem;
+        }
+        setDisplayTime(time);
+    };
+
+    useEffect(() => {
+        updateTime(); // Initial update
+        const interval = setInterval(updateTime, 10 * 1000);
+        return () => clearInterval(interval);
+    }, [onlyTime, onlyDay]);
+
+    return <span>{displayTime}</span>;
+};
+
+// Prevent server-side rendering for this component
+export default dynamic(() => Promise.resolve(Clock), {
+    ssr: false
+});
